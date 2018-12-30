e15574c7a4f8465b5e599e1eecf3a6ec2e884e40
vikram240 <31476140+vikram240@users.noreply.github.com>
2018-12-30 12:11:59 +0530

    Add files via upload

---
+++ sentiment_mod_Test.py
@@ -0,0 +1,6 @@
+import sentiment_mod as s
+
+print(s.sentiment("This movie was awesome! The acting was great, plot was wonderful, and there were pythons...so yea!"))
+print(s.sentiment("This movie was utter junk. There were absolutely 0 pythons. I don't see what the point was at all. Horrible movie, 0/10"))
+
+print(s.sentiment("I can find some mistakes in the film. But I love the acting of Nawajuddin Sidhiki. I think the movie was good till first half but second half was bit lengthy."))
